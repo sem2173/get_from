a4212ee2575a2a231e984038a3f3fa946e38a99e
simplonco <semia.ladjal@gmail.com>
2015-01-22 21:36:28 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+from flask import Flask, render_template, request
+import requests
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def get_movies():
+    if request.method =='POST':
+        movieapi=request.form['film']
+        return render_template('index.html', movieapi=movieapi)    
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
